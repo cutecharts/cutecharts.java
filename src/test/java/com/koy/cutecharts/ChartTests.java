c695c4ee55fe935f337e5adb1091c73500316cba
koooooo-7 <369491420@qq.com>
2020-04-02 22:01:15 +0800

    [unit test]

--- src/test/java/com/koy/cutecharts/ChartTests.java
+++ src/test/java/com/koy/cutecharts/ChartTests.java
@@ -1,5 +1,15 @@
 package com.koy.cutecharts;
 
+import com.koy.cutecharts.core.ChartBuilder;
+import com.koy.cutecharts.enums.ChartType;
+import com.koy.cutecharts.options.BaseOptions;
+import com.koy.cutecharts.options.GlobalOptions;
+import org.junit.Assert;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import static org.mockito.Mockito.when;
+
 /**
  * @Description
  * @Auther Koy  https://github.com/Koooooo-7
@@ -8,4 +18,23 @@ package com.koy.cutecharts;
 public class ChartTests {
 
     //TODO: Unit Test
+
+    @Test
+    public void testChartBuilderTypeCheck() {
+        BaseOptions options = Mockito.mock(BaseOptions.class);
+        when(options.getChartType()).thenReturn(ChartType.LINE);
+        ChartBuilder.newChart newChart = new ChartBuilder.newChart(ChartType.BAR);
+        Assert.assertThrows(IllegalArgumentException.class, () -> {
+            newChart.setOptions(options);
+        });
+    }
+
+    @Test
+    public void testGlobalDefaultOptions() {
+        GlobalOptions globalOptions = new GlobalOptions();
+        Assert.assertEquals("800px", globalOptions.getWidth());
+        Assert.assertEquals("600px", globalOptions.getHeight());
+    }
+
+
 }
